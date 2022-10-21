2ee7d633031dc6572264b02cadf9963e279f3faa
“[Peter <pwdrysdale@gmail.com>
2022-10-20 21:57:58 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pwdrysdale/go-todos
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,16 @@
+// basic go web server that will listen on port 8080
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World!")
+	})
+
+	http.ListenAndServe( ":8080", nil)
+}
